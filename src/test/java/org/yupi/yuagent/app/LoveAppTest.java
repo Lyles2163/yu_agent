49e0b0eeab74128dd526d8de4863257b7976aebd
Administrator <13631155285@163.com>
2026-06-29 21:46:17 +0800

    6.29基础记忆对话实现测试功能

---
+++ src/test/java/org/yupi/yuagent/app/LoveAppTest.java
@@ -0,0 +1,29 @@
+package org.yupi.yuagent.app;
+
+import cn.hutool.core.lang.UUID;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LoveAppTest {
+    @Resource LoveApp loveApp;
+
+    @Test
+    void testDoChat() {
+       String chaId= UUID.randomUUID().toString();
+       //第一轮测试
+       String message="你好,我叫Leon";
+       String answer= loveApp.doChat(message,chaId);
+       //第二轮测试
+        message="我的初恋是Ada";
+        answer= loveApp.doChat(message,chaId);
+        Assertions.assertNotNull( answer);
+        //第三轮测试
+        message="我的初恋叫什么来着？回复我名字";
+        answer= loveApp.doChat(message,chaId);
+        Assertions.assertNotNull( answer);
+
+    }
+}
